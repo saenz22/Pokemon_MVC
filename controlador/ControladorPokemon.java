6f8a59619892725daff620bf44ffde0bd91f3ce8
agughost1234 <agudelo.samuel@correounivalle.edu.co>
2025-05-10 14:30:50 -0500

    cuadrando errores de archivos

--- controlador/ControladorPokemon.java
+++ controlador/ControladorPokemon.java
@@ -1,6 +1,7 @@
-import vista.VistaPokemonGUI;
 
 package controlador;
+
+
 
 // import vista.VistaPokemonGUI; // Uncomment this line if the vista package and VistaPokemonGUI class exist
 
--- vista/VistaPokemonGUI.java
+++ vista/VistaPokemonGUI.java
@@ -1,4 +1,4 @@
-package vista;t 
+
 
 import javax.swing.BorderFactory;
 import javax.swing.JFrame;
